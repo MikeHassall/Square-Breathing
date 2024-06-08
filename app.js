257450c33622bb5b60e9ed94921ac10fabf7e90b
MikeHassall <michaelhassall@hotmail.com>
2024-06-08 10:48:16 +1000

    Added start stop button
    - added start, stop functionality
    - updated styles

--- app.js
+++ app.js
@@ -1,42 +1,45 @@
+// version 1.0
 const main_canvas = document.getElementById("mainCanvas");
 const ctx = main_canvas.getContext("2d");
 
 main_canvas.width = Math.floor(window.innerWidth);
-// add room for page title
-main_canvas.height = Math.floor(window.innerHeight) - 100;
+// add room for page title and button
+main_canvas.height = Math.floor(window.innerHeight) - 150;
 
+is_running = false;
 start_time = Date.now();
 
 const square = {
     start_x: 0,
     start_y: 0,
-    top_side_x1: -1,
-    top_side_y1: -1,
-    top_side_x2: -1,
-    top_side_y2: -1,
-    top_side_colour: "rgb(174, 198, 229)",
-
-    right_side_x1: -1,
-    right_side_y1: -1,
-    right_side_x2: -1,
-    right_side_y2: -1,
-    right_side_colour: "rgb(205, 198, 224)",
-
-    bottom_side_x1: -1,
-    bottom_side_y1: -1,
-    bottom_side_x2: -1,
-    bottom_side_y2: -1,
-    bottom_side_colour: "rgb(245, 198, 205)",
-
-    left_side_x1: -1,
-    left_side_y1: -1,
-    left_side_x2: -1,
-    left_side_y2: -1,
-    left_side_colour: "rgb(167, 221, 221)",
+    // top side
+    ts_x1: -1,
+    ts_y1: -1,
+    ts_x2: -1,
+    ts_y2: -1,
+    ts_colour: "rgb(174, 198, 229)",
+    // right side
+    rs_x1: -1,
+    rs_y1: -1,
+    rs_x2: -1,
+    rs_y2: -1,
+    rs_colour: "rgb(205, 198, 224)",
+    // bottom side
+    bs_x1: -1,
+    bs_y1: -1,
+    bs_x2: -1,
+    bs_y2: -1,
+    bs_colour: "rgb(245, 198, 205)",
+    // left side
+    ls_x1: -1,
+    ls_y1: -1,
+    ls_x2: -1,
+    ls_y2: -1,
+    ls_colour: "rgb(167, 221, 221)",
 
     text_colour: "rgb(241, 186, 141)",
     background_colour: "rgb(255, 255, 255)",
-    circle_colour: "rgb(174, 198, 229)",
+    circle_colour: this.ts_colour,
 
     direction: -1,
     run_counter: 0,
@@ -49,26 +52,29 @@ const square = {
 
     draw: function () {
 
-        ctx.font = "30px Arial";
+        ctx.font = "50px Arial";
         ctx.fillStyle = this.text_colour;
         ctx.textAlign = "center";
 
         switch (square.direction) {
 
             case 0:
-                ctx.fillText("breath in slowly", main_canvas.width / 2, main_canvas.height / 2);
+                ctx.fillText("breath in", main_canvas.width / 2, (main_canvas.height / 2)-30);
+                ctx.fillText("slowly", main_canvas.width / 2, (main_canvas.height / 2)+30);
                 break;
             case 1:
                 ctx.fillText("hold it in", main_canvas.width / 2, main_canvas.height / 2);
                 break;
             case 2:
-                ctx.fillText("breath out slowly", main_canvas.width / 2, main_canvas.height / 2);
+                ctx.fillText("breath out", main_canvas.width / 2, (main_canvas.height / 2)-30);
+                ctx.fillText("slowly", main_canvas.width / 2, (main_canvas.height / 2)+30);
+
                 break;
             case 3:
                 ctx.fillText("pause", main_canvas.width / 2, main_canvas.height / 2);
                 break;
             default:
-                ctx.fillText("Get Ready...", main_canvas.width / 2, main_canvas.height / 2);
+                ctx.fillText("get ready...", main_canvas.width / 2, main_canvas.height / 2);
                 break;
         }
 
@@ -95,40 +101,40 @@ const square = {
             ctx.closePath();
 
             ctx.beginPath();
-            ctx.strokeStyle = this.top_side_colour;
+            ctx.strokeStyle = this.ts_colour;
             ctx.lineWidth = 3;
-            ctx.moveTo(this.top_side_x1, this.top_side_y1);
-            ctx.lineTo(this.top_side_x2, this.top_side_y2);
+            ctx.moveTo(this.ts_x1, this.ts_y1);
+            ctx.lineTo(this.ts_x2, this.ts_y2);
             ctx.stroke();
             ctx.closePath();
         }
 
         if (this.direction >= 1) {
             ctx.beginPath();
-            ctx.strokeStyle = this.right_side_colour;
+            ctx.strokeStyle = this.rs_colour;
             ctx.lineWidth = 3;
-            ctx.moveTo(this.right_side_x1, this.right_side_y1);
-            ctx.lineTo(this.right_side_x2, this.right_side_y2);
+            ctx.moveTo(this.rs_x1, this.rs_y1);
+            ctx.lineTo(this.rs_x2, this.rs_y2);
             ctx.stroke();
             ctx.closePath();
         }
 
         if (this.direction >= 2) {
             ctx.beginPath();
-            ctx.strokeStyle = this.bottom_side_colour;
+            ctx.strokeStyle = this.bs_colour;
             ctx.lineWidth = 3;
-            ctx.moveTo(this.bottom_side_x1, this.bottom_side_y1);
-            ctx.lineTo(this.bottom_side_x2, this.bottom_side_y2);
+            ctx.moveTo(this.bs_x1, this.bs_y1);
+            ctx.lineTo(this.bs_x2, this.bs_y2);
             ctx.stroke();
             ctx.closePath();
         }
 
         if (this.direction >= 3) {
             ctx.beginPath();
-            ctx.strokeStyle = this.left_side_colour;
+            ctx.strokeStyle = this.ls_colour;
             ctx.lineWidth = 3;
-            ctx.moveTo(this.left_side_x1, this.left_side_y1);
-            ctx.lineTo(this.left_side_x2, this.left_side_y2);
+            ctx.moveTo(this.ls_x1, this.ls_y1);
+            ctx.lineTo(this.ls_x2, this.ls_y2);
             ctx.stroke();
             ctx.closePath();
         }
@@ -160,13 +166,13 @@ const square = {
                 this.current_x = this.start_x;
                 this.current_y = this.start_y;
 
-                this.top_side_x1 = this.start_x;
-                this.top_side_y1 = this.start_y;
-                this.top_side_x2 = this.start_x;
-                this.top_side_y2 = this.start_y;
+                this.ts_x1 = this.start_x;
+                this.ts_y1 = this.start_y;
+                this.ts_x2 = this.start_x;
+                this.ts_y2 = this.start_y;
 
-                this.circle_colour = this.top_side_colour;
-                this.text_colour = this.top_side_colour
+                this.circle_colour = this.ts_colour;
+                this.text_colour = this.ts_colour
 
                 start_time = Date.now();
             }
@@ -174,18 +180,18 @@ const square = {
         else if (this.direction == 0) {
             // right
             this.current_x = this.current_x + this.increment_size;
-            this.top_side_x2 = this.current_x;
+            this.ts_x2 = this.current_x;
             // always time this path to adjust the gridsize/time
             var milliseconds = Date.now() - start_time;
             if (milliseconds >= 4000) {
                 this.max_size = this.current_x - this.start_x;
                 // now we can draw the square based on time
-                this.right_side_x1 = this.current_x;
-                this.right_side_x2 = this.current_x;
-                this.right_side_y1 = this.start_y;
-                this.right_side_y2 = this.start_y;
-                this.circle_colour = this.right_side_colour;
-                this.text_colour = this.right_side_colour;
+                this.rs_x1 = this.current_x;
+                this.rs_x2 = this.current_x;
+                this.rs_y1 = this.start_y;
+                this.rs_y2 = this.start_y;
+                this.circle_colour = this.rs_colour;
+                this.text_colour = this.rs_colour;
 
                 this.direction += 1;
             }
@@ -193,56 +199,113 @@ const square = {
             // down
             let max_check = this.max_size + this.start_y;
             this.current_y = this.current_y + this.increment_size;
-            this.right_side_y2 = this.current_y;
+            this.rs_y2 = this.current_y;
 
             if (this.current_y >= max_check) {
-                this.bottom_side_x1 = this.current_x;
-                this.bottom_side_x2 = this.current_x;
-                this.bottom_side_y1 = this.current_y;
-                this.bottom_side_y2 = this.current_y;
-                this.circle_colour = this.bottom_side_colour;
-                this.text_colour = this.bottom_side_colour;
+                this.bs_x1 = this.current_x;
+                this.bs_x2 = this.current_x;
+                this.bs_y1 = this.current_y;
+                this.bs_y2 = this.current_y;
+                this.circle_colour = this.bs_colour;
+                this.text_colour = this.bs_colour;
 
                 this.direction++;
             }
         } else if (this.direction == 2) {
             // left
             this.current_x = this.current_x - this.increment_size;
-            this.bottom_side_x2 = this.current_x;
+            this.bs_x2 = this.current_x;
             if (this.current_x <= this.start_x) {
-                this.left_side_x1 = this.start_x;
-                this.left_side_x2 = this.start_x;
-                this.left_side_y1 = this.current_y;
-                this.left_side_y2 = this.current_y;
-                this.circle_colour = this.left_side_colour;
-                this.text_colour = this.left_side_colour;
+                this.ls_x1 = this.start_x;
+                this.ls_x2 = this.start_x;
+                this.ls_y1 = this.current_y;
+                this.ls_y2 = this.current_y;
+                this.circle_colour = this.ls_colour;
+                this.text_colour = this.ls_colour;
 
                 this.direction++;
             }
         } else {
             // up
             this.current_y = this.current_y - this.increment_size;
-            this.left_side_y2 = this.current_y;
+            this.ls_y2 = this.current_y;
             if (this.current_y <= this.start_y) {
-                this.top_side_x1 = this.start_x;
-                this.top_side_x2 = this.start_x;
-                this.top_side_y1 = this.start_y;
-                this.top_side_y2 = this.start_y;
-                this.circle_colour = this.top_side_colour;
-                this.text_colour = this.top_side_colour;
+                this.ts_x1 = this.start_x;
+                this.ts_x2 = this.start_x;
+                this.ts_y1 = this.start_y;
+                this.ts_y2 = this.start_y;
+                this.circle_colour = this.ts_colour;
+                this.text_colour = this.ts_colour;
 
                 this.direction = 0;
                 // reset the timer
                 start_time = Date.now();
             }
         }
+    },
+    
+    reset: function () {
+        this.start_x = 0;
+        this.start_y = 0;
+        this.ts_x1 = -1;
+        this.ts_y1 = -1;
+        this.ts_x2 = -1;
+        this.ts_y2 = -1;
+        
+        this.rs_x1= -1;
+        this.rs_y1= -1;
+        this.rs_x2= -1;
+        this.rs_y2= -1;
+    
+        this.bs_x1= -1;
+        this.bs_y1= -1;
+        this.bs_x2= -1;
+        this.bs_y2= -1;
+    
+        this.ls_x1= -1;
+        this.ls_y1= -1;
+        this.ls_x2= -1;
+        this.ls_y2= -1;
+
+
+        this.text_colour = "rgb(241, 186, 141)";
+        this.background_colour = "rgb(255, 255, 255)";
+        this.circle_colour = "rgb(174, 198, 229)";
+    
+        this.direction = -1;
+        this.run_counter = 0;
+        this.current_x = 0;
+        this.current_y = 0;
+        this.max_size = -1;
+        this.increment_size = 0;
+        this.increments = 0;
     }
 }
 
-function main() {
-    ctx.clearRect(0, 0, main_canvas.width, main_canvas.height);
-    square.update();
-    square.draw();
-    requestAnimationFrame(main);
+function startStopButtonClicked() {
+    btn = document.getElementById('start-stop-button');
+    is_running = !is_running;
+    if (is_running) {
+        btn.innerHTML = "Stop";
+
+        start_time = Date.now();
+        requestAnimationFrame(runProcess);
+    } else {
+        btn.innerHTML = "Start";
+    }
 }
-requestAnimationFrame(main);
+
+function runProcess() {
+    ctx.clearRect(0, 0, main_canvas.width, main_canvas.height);
+
+    if (is_running) {
+        square.update();
+        square.draw();
+        requestAnimationFrame(runProcess);
+    }else{
+        main_canvas.width = Math.floor(window.innerWidth);
+        main_canvas.height = Math.floor(window.innerHeight) - 150;
+        square.reset();
+
+    }
+}
